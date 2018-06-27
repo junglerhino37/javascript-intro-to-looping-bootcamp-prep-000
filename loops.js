0c106c0062641f030d54e5cb944e3dd165f54ec5
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-27 04:54:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,6 +3,10 @@ forLoop(array)
 console.log(array)
 function forLoop(x){
   for(let i=0; i<25; i++){
-    if (i===1)  x.push("I am ${i} strange loops.")
+    if (i===1)  {
+      x.push("I am 1 strange loop.")
+    } else {
+      x.push("I am ${i} strange loops.")
+    }
   }
 }
